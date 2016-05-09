6a5f6cdc4afbc6ae6e05b04a8fa258ea7f184f52
cptran777 <cptran14@gmail.com>
2016-05-08 23:41:56 -0500

    parseJSON first pass v0.6

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -58,13 +58,103 @@ the provided grammar. Within each function, the string should be traversed to
 * interpret the value
 
 
-
-
-
-
-
 */
 
-var parseJSON = function(json) {
-  // your code goes here
-};
+function parseJSON(jString){
+  
+  var output = jString;
+  
+  // Creating an object to hold and track the overall JSON string:
+  
+  var stringTrack = {
+    json: jString,
+    position: -1,
+    nextChar: function(){
+      this.position += this.position < this.json.length ? 1 : 0;
+      return this.json[this.position];
+    },
+    prevChar: function(){
+      this.position -= 1;
+      return this.json[this.position];
+    },
+    remainingJString: function(){
+      return this.json.slice(this.position);
+    }
+  };
+  
+  // The function that will recursively be called to parse the JSON string
+  
+  var parser = function(){
+    
+    var output;
+    var nextParse = stringTrack.nextChar();
+    
+    console.log(nextParse);
+    
+    var numberBuilder = function(){
+      output = nextParse;
+      var nextNumCheck = stringTrack.nextChar();
+      while(nextNumCheck.match(/[0-9]/)){
+        output += nextNumCheck;
+        nextNumCheck = stringTrack.nextChar();
+      }
+      stringTrack.prevChar();
+      output = Number(output);
+    };    
+    
+    // For booleans, null, undefined, NaN's
+    var valueBuilder = function(){
+      
+    };
+    
+    var stringBuilder = function(){
+      var nextChar = stringTrack.nextChar();
+      console.log("string build: " + nextChar);
+      while(nextChar !== "\"" && stringTrack.position < stringTrack.json.length){
+        output += nextChar;
+        nextChar = stringTrack.nextChar();
+      }
+    };
+    
+    var arrayBuilder = function(){
+      output.push(parser());
+      if(stringTrack.nextChar() === ","){
+        arrayBuilder();
+      }
+    };
+    
+    var objectBuilder = function(){
+      var buildKey = parser();
+      stringTrack.nextChar();
+      output[buildKey] = parser();
+      if(stringTrack.nextChar() === ","){
+        objectBuilder();
+      }
+    };
+    
+    var mappingFunction = function(){
+      if(nextParse === "{"){
+        output = {};
+        objectBuilder();
+      } else if(nextParse === "\""){
+        output = "";
+        stringBuilder();
+      } else if(nextParse === "["){
+        output = [];
+        arrayBuilder();
+      } else if(nextParse.match(/[0-9]/)){
+        numberBuilder();
+      } else {
+        valueBuilder();
+      }
+    };
+    
+    mappingFunction();
+    
+    return output;
+    
+  };
+  
+  return parser();
+  
+}
